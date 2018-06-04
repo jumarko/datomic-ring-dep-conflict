91acea4b307dfd466cb84b2f8bfe2229b7fb3a71
Juraj Martinka <jumarko@gmail.com>
2018-06-04 10:49:33 +0200

    Fix jetty dependencies and add proper datomic client config.

--- src/test/core.clj
+++ src/test/core.clj
@@ -1,7 +1,23 @@
 (ns test.core
-  (:gen-class))
+  (:require [datomic.client.api :as d]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;; hardcoded config
+(def cfg
+  {:server-type :cloud
+   :region "eu-central-1" ;; e.g. us-east-1
+   :system "juraj-datomic-cloud"
+   :query-group "juraj-datomic-cloud"
+   :endpoint "http://entry.juraj-datomic-cloud.eu-central-1.datomic.net:8182/"
+   :proxy-port 8182
+   :creds-profile "juraj-datomic-cloud"})
+
+(defn connect-client []
+  (d/client cfg))
+
+(defn handler [request]
+  (let [db (d/db (d/connect (connect-client) {:db-name "movies"}))
+        all-titles-q '[:find ?movie-title 
+                       :where [_ :movie/title ?movie-title]]]
+    {:status 200
+     :headers {"Content-Type" "text/plain"}
+     :body (pr-str (d/q all-titles-q db))}))
