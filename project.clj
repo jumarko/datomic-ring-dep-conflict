9a56430ede33d7d531b57de86dd4bfbdf7da7690
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2018-06-03 21:47:25 -0400

    Add d/client call to show error

--- project.clj
+++ project.clj
@@ -6,18 +6,32 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.datomic/client-cloud "0.8.52"
                   :exclusions [org.eclipse.jetty/jetty-client
-                                org.eclipse.jetty/jetty-http
-                                org.eclipse.jetty/jetty-util]]
+                               org.eclipse.jetty/jetty-http
+                               org.eclipse.jetty/jetty-util]]
+                 [environ "1.1.0"]
+                 [cheshire "5.8.0"]
                  [ring "1.7.0-RC1"]
-                 ;; [ring/ring-defaults "0.2.3"]
-                 ;; [ring/ring-json "0.4.0"]
-                 ;; [ring-cors "0.1.9"]
-                 ;; [compojure "1.6.1"]
-                 ;; [hickory "0.7.0"]
-                 ;; [markdown-clj "0.9.94"]
+                 [ring/ring-defaults "0.2.3"]
+                 [ring/ring-json "0.4.0"]
+                 [ring-cors "0.1.9"]
+                 [compojure "1.6.1"]
+                 [hickory "0.7.0"]
+                 [markdown-clj "0.9.94"]
                  ]
   :ring {:handler test.core/handler}
   :plugins [[lein-ring "0.12.4"]]
   :main ^:skip-aot test.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {
+   ;; :production {:env {:db-name "production"}
+   ;;              :dependencies [[javax.serverlet/servlet-api "2.5"]
+   ;;                             [ring/ring-mock "0.3.2"]]}
+   :dev {:env {:db-name "development"}
+         :dependencies [
+                        ;; [javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]
+                        ]}
+   ;; :test {:env {:testing true}
+   ;;        :dependencies [[javax.servlet/servlet-api "2.5"]
+   ;;                       [ring/ring-mock "0.3.2"]]}
+   })
--- src/test/core.clj
+++ src/test/core.clj
@@ -1,6 +1,16 @@
-(ns test.core)
+(ns test.core
+  (:require [datomic.client.api :as d]
+            [clojure.java.io :as io]))
+
+(def resource io/resource)
+
+(defn read-cfg []
+   (read-string (slurp (resource "db-config.edn"))))
+
+(defn connect-client []
+  (d/client (read-cfg)))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body (:remote-addr request)})
+   :body (connect-client)})
